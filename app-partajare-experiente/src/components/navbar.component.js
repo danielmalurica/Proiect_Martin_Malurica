8bb55840ba04bafc12c341c22290a0e8f8b027da
Dani. Malurica <danielmalurica@gmail.com>
2020-12-13 02:06:30 +0200

    frontend

---
+++ app-partajare-experiente/src/components/navbar.component.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component{
+    render() {
+        return (
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+                <Link to="/" className="navbar-brand">Feedback Transport</Link>
+                <div className="collpase navbar-collapse">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="navbar-item">
+                            <Link to="/" className="nav-link">Calatorii</Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link to="/create" className="nav-link">Adauga Calatorie Noua</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
